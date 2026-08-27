84b357acb15670ef19473963a74583243ebc0f49
Jackson Brink <brinkjackson101@gmail.com>
2026-08-27 12:04:09 -0400

    Update YouTube link

--- yes.bat
+++ yes.bat
@@ -1,1 +1,1 @@
-start chrome.exe "https://youtu.be/jqDGK_UjfFI?si=NMjZUDFzyuoQBN52"
+start chrome.exe "https://www.youtube-nocookie.com/embed/wNsr2jCbMQY?playlist=wNsr2jCbMQY&autoplay=1&iv_load_policy=3&loop=1&start="
