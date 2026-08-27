a3452ef83e728db9cc12019874f1df77f83f1784
Jackson Brink <brinkjackson101@gmail.com>
2026-08-27 12:09:21 -0400

    Change YouTube link to standard format

--- yes.bat
+++ yes.bat
@@ -1,1 +1,1 @@
-start chrome.exe "https://www.youtube-nocookie.com/embed/wNsr2jCbMQY?playlist=wNsr2jCbMQY&autoplay=1&iv_load_policy=3&loop=1&start="
+start chrome.exe "https://www.youtube.com/watch?v=wNsr2jCbMQY"
